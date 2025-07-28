8c1ed84db59410a71fa39bf9d817462be30dc7dc
Reddy <bsuryateja777@gmail.com>
2025-07-28 15:35:32 +0530

    office

--- client/src/userContext.jsx
+++ client/src/userContext.jsx
@@ -7,22 +7,27 @@ export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
   const [ready, setReady] = useState(false);
 
+
   useEffect(() => {
     if (!user) {
-      axios.get('/profile')
+      axios.get('https://rentease-backend-5p7h.onrender.com/profile', {
+        withCredentials: true,
+      })
         .then(({ data }) => {
           setUser(data);
           setReady(true);
         })
         .catch((err) => {
           console.error("Profile fetch error:", err.message);
+          setReady(true);
         });
     }
   }, [user]);
-  
+
+
 
   return (
-    <UserContext.Provider value={{ user, setUser,ready }}>
+    <UserContext.Provider value={{ user, setUser, ready }}>
       {children}
     </UserContext.Provider>
   );
