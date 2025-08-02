1acd138cf4f61a2491694b6b17bdf5e78e4bd281
bsuryateja777 <140967103+bsuryateja777@users.noreply.github.com>
2025-08-02 12:53:13 +0530

    Update placeRoutes.js

--- api/routes/placeRoutes.js
+++ api/routes/placeRoutes.js
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 import { createPlace, updatePlace, getUserPlaces, getPlace, deletePlace, getAllPlaces } from '../controllers/placeController.js';
+import multer from 'multer';
+import path from 'path';
 
 import { verifyToken } from '../middlewares/authMiddleware.js';
+
+const upload = multer({
+  dest: path.join(__dirname, '..', 'uploads', 'user-places')
+});
 
 const router = Router();
 
@@ -13,5 +19,8 @@ router.delete('/my-accomodations/:id', deletePlace);
 
 router.get('/places', getAllPlaces);
 router.get('/all-places/:id', getPlace);  // Optional: You may remove if duplicate of above
+
+router.post('/upload-by-link', uploadByLink);
+router.post('/upload', upload.array('photos', 10), uploadPhotos);
 
 export default router;
