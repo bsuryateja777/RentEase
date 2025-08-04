197232bf3bfae6ade3455746dce4a2db78e0d503
Reddy <bsuryateja777@gmail.com>
2025-08-04 19:03:44 +0530

    office public_id

--- api/controllers/cloudinaryController.js
+++ api/controllers/cloudinaryController.js
@@ -1,39 +1,22 @@
 // controllers/cloudinaryController.js
 import cloudinary from '../config/cloudinary.js';
-import axios from 'axios';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { tmpdir } from 'os';
-import { v4 as uuid } from 'uuid';
 
-// Handle __dirname in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// POST /upload-to-cloudinary
 export const uploadToCloudinaryByLink = async (req, res) => {
   try {
     const { image } = req.body;
+
     if (!image.startsWith('http')) {
       return res.status(400).json({ error: 'Invalid image URL' });
     }
-    const uniqueName = `temp_${uuid()}.jpg`;
-    const tempPath = path.join(tmpdir(), uniqueName);
 
-
-    // Step 1: Upload to Cloudinary
-    const result = await cloudinary.uploader.upload(tempPath, {
+    const result = await cloudinary.uploader.upload(image, {
       folder: 'RentEase',
-      upload_preset: 'RentEase', // must be unsigned
+      resource_type: 'image',
     });
 
-    // Step 2: Cleanup and return result
-    fs.unlinkSync(tempPath); // remove temp file
-    res.json({ url: result.secure_url, public_id : result.public_id });
-    res.send({ url: result.secure_url, public_id : result.public_id });
+    res.json({ url: result.secure_url, public_id: result.public_id });
   } catch (error) {
-    console.error('Cloudinary upload failed:', error.message);
-    res.status(500).json({ error: 'Failed to upload image' });
+    console.error('Cloudinary direct URL upload failed:', error.message);
+    res.status(500).json({ error: 'Failed to upload image from URL' });
   }
 };
