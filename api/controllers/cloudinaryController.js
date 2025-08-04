6b602d2ac6722a422139e2a751c06af0da4be1a9
Reddy <bsuryateja777@gmail.com>
2025-08-05 00:54:12 +0530

    office public_id

--- api/controllers/cloudinaryController.js
+++ api/controllers/cloudinaryController.js
@@ -9,23 +9,19 @@ export const uploadToCloudinary = async (req, res) => {
       return res.status(400).json({ error: 'Invalid image URL' });
     }
 
-    const result = await cloudinary.uploader
-      .upload(
-        image,
-        {
-          public_id: '1',
-        }
-      )
+    
 
-
-    // const result = await cloudinary.uploader.upload(image, {
-    //   folder: 'RentEase-Signed',
-    //   resource_type: 'image',
-    // });
+    const result = await cloudinary.uploader.upload(image, {
+      upload_preset: 'RentEase-Unsigned',
+      folder: 'RentEase-Unsigned',
+      resource_type: 'image',
+    });
 
     res.json({ url: result.secure_url, public_id: result.public_id });
+    console.log('url: ', result.secure_url)
+    console.log('public_id: ', result.public_id)
   } catch (error) {
-    console.error('Cloudinary direct URL upload failed:', error.message);
+    console.error('Cloudinary direct URL upload failed:', error);
     res.status(500).json({ error: 'Failed to upload image from URL' });
   }
 };
