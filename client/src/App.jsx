0c5a46798581f2eb851c925d560861aaa79f87f4
bsuryateja777 <bsuryateja777@gmail.com>
2025-12-18 06:30:13 +0530

    Configuring front-end for Amplify Deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ import MyGuestsPage from './Pages/MyGuestsPage.jsx';
 import SinglePlacePage from './Pages/SinglePlacePage.jsx'
 
 
-axios.defaults.baseURL = 'https://rentease-backend-5p7h.onrender.com';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 // axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true;
 
